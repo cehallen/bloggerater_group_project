8b3d1f5cf9e26f4da39220f5bcc37e37e000963e
Daniel Greenberg <dgreenbe777@gmail.com>
2014-03-31 15:46:07 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloggerater::Application.config.secret_key_base = 'e6086211894dd3d2888a898470f7c5bb98f8173335e9cb8a9ab2c296ca31f6257848be70a4fe2405543bb1f2c1c19bf8a1898a4cf7fd72fbf74acbe49b84c7f3'
