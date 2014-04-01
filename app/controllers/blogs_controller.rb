fead627e58ee127766aef6b6654e2326ab0c8c8b
Robel <robeltadesse01@gmail.com>
2014-04-01 14:33:28 -0400

    create feature added

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,6 +9,22 @@ class BlogsController < ApplicationController
   end
 
   def create
+    # @user = current_user
+    # @blog = Blog.new(blog_params)
+    # @user.blogs << @blog
+    @blog = Blog.new(blog_params)
+    @blog.user = current_user
+
+    if @blog.save
+      redirect_to blogs_path, notice: 'Blog Added to Listing'
+    else
+      render :new
+    end
   end
 
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :description, :url)
+  end
 end
--- spec/features/blog_post_creation_spec.rb
+++ spec/features/blog_post_creation_spec.rb
@@ -10,20 +10,37 @@ feature 'user trying to make blog post', %q{
   # I have to enter in a email
   # I have to enter in a password
   # What happens when you're not signed in?
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+
+    visit new_user_session_path
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: @user.password
+    within "#new_user" do
+      click_on "Sign in"
+    end
+  end
 
   context 'filling out blog form information' do
 
-    before(:each) do
-      @user = FactoryGirl.create(:user)
+    it 'allows signed in user to post a blog if required attributes given' do
+      visit new_blog_path
+      fill_in 'blog[title]', with: 'A title'
+      fill_in 'blog[url]', with: 'www.blogs.com'
+
+      click_on 'Create Blog'
+
+      expect(page).to have_content('Blog Added to Listing')
+
+
     end
 
-    it 'requires a title' do
-    end
+    it 'gives errors if required fields are missing' do
+      visit new_blog_path
 
-    it 'requires a url' do
-    end
+      click_on 'Create Blog'
 
-    it 'can optionally have a description' do
+      expect(page).to_not have_content("Blog Added to Listing")
     end
 
   end
@@ -31,6 +48,17 @@ feature 'user trying to make blog post', %q{
   context 'printing out blog post' do
 
     it 'prints blog post information on index page after submission' do
+      visit new_blog_path
+
+      fill_in 'blog[title]', with: 'A title'
+      fill_in 'blog[url]', with: 'www.blogs.com'
+
+      click_on 'Create Blog'
+
+      expect(page).to have_content('Blog Added to Listing')
+      expect(page).to have_content('List of Blogs')
+      expect(page).to have_content('A title')
+
     end
 
   end
