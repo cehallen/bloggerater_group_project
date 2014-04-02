49e58d2afa0e0277e184520484a603913d828a65
Daniel Greenberg <dgreenbe777@gmail.com>
2014-04-02 14:02:50 -0400

    nothing really changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401193009) do
+ActiveRecord::Schema.define(version: 20140401204226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20140401193009) do
     t.string   "image_url"
     t.text     "description"
     t.integer  "user_id",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ratings", force: true do |t|
+    t.integer  "rating",     null: false
+    t.integer  "rater_id",   null: false
+    t.integer  "blog_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
